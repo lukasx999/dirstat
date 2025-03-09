420ec1c2bdb0093a0d274500bcc5a54cac4761c8
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-03-09 22:00:08 +0100

    added code

--- main.zig
+++ main.zig
@@ -1,8 +1,71 @@
-const std = @import("std");
+const std   = @import("std");
+const ally  = std.heap.page_allocator;
+const fs    = std.fs;
+const print = std.debug.print;
 
 
-pub fn main() void {
+fn openDir(dir: []const u8) !fs.Dir {
+    const path = try fs.realpathAlloc(ally, dir);
+    defer ally.free(path);
 
-    std.debug.print("Hello, World!\n", .{});
+    return fs.openDirAbsolute(path, .{ .iterate=true });
+}
+
+const DirInfo = struct {
+    entryCount:   u32,
+    hiddenCount:  u32,
+    dirCount:     u32,
+    fileCount:    u32,
+    symlinkCount: u32,
+
+    const Self = @This();
+
+    fn new() Self {
+        return .{
+            .entryCount   = 0,
+            .hiddenCount  = 0,
+            .dirCount     = 0,
+            .fileCount    = 0,
+            .symlinkCount = 0,
+        };
+    }
+
+    fn getDirInfo(dir: fs.Dir) !Self {
+        var walker = try dir.walk(ally);
+        defer walker.deinit();
+
+        var info = Self.new();
+
+        while (try walker.next()) |entry| {
+
+            const Kind = fs.Dir.Entry.Kind;
+            switch (entry.kind) {
+                Kind.directory => info.dirCount     += 1,
+                Kind.sym_link  => info.symlinkCount += 1,
+                Kind.file      => info.fileCount    += 1,
+                else => {},
+            }
+
+            info.entryCount += 1;
+            if (entry.basename[0] == '.')
+            info.hiddenCount += 1;
+        }
+
+        return info;
+
+    }
+
+};
+
+
+pub fn main() !void {
+
+    var dir = try openDir("./test/");
+    defer dir.close();
+
+    const info = try DirInfo.getDirInfo(dir);
+
+    print("info: {}\n", .{ info });
+
 
 }
